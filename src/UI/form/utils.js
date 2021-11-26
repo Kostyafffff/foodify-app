9f4f3b99cba691c05499a00951f2e92854143bef
Kostya <konstantin.furmanov@gmail.com>
2021-11-26 16:38:01 +0200

    fix empty spaces form

--- src/UI/form/utils.js
+++ src/UI/form/utils.js
@@ -1,1 +1,1 @@
-export const isFormValid = (...params) => Array.from(params).every(it => it.length > 0);
+export const isFormValid = (...params) => Array.from(params).every(it => it.length > 0 && it.trim().length > 0);
